8e726cddfa13ae65769ae749a5f8662280fc9d98
A. Zain <57663136+gbadnanzain@users.noreply.github.com>
2022-11-26 22:41:47 +0700

    First Commit

---
+++ lib/widget/HomeAppBar.dart
@@ -0,0 +1,34 @@
+import 'package:flutter/material.dart';
+
+class HomeAppBar extends StatelessWidget {
+  const HomeAppBar({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      color: Colors.white,
+      padding: const EdgeInsets.all(25),
+      child: Row(
+        children: [
+          Icon(
+            Icons.sort,
+            size: 30,
+            color: const Color(0xFF4C53A5),
+          ),
+          Padding(
+            padding: EdgeInsets.only(
+              left: 20,
+            ),
+            child: Text(
+              "HDI Shop",
+              style: const TextStyle(
+                fontSize: 23,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
